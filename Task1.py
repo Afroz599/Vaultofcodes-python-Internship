8961feb813aa85d9c4059ae982be04fe4cb35820
Mohammad Afroz <afrozmohommad022@gmail.com>
2025-08-06 15:23:22 +0530

    Initial commit

---
+++ Task1.py
@@ -0,0 +1,42 @@
+#VaultofCodes | python  AUG 25 | Batch code:pa2y63ey
+#Assignment-1 :Code Review and Error Correction 
+#1.Code Snippert 1
+def add_numbers(a,b):
+    return a+b
+print(add_numbers(5,10))
+#2.Code Snippet 2
+name ="Alice"
+print("Hello, "+name)
+#3.Code Snippet 3
+for i in range(5):
+    print("Number:",i)
+#4.Code Snippet 4
+my_list=[1,2,3,4,5]
+print("The fifth element is:",my_list[4])
+#5.Code Snippet 5
+def greet(name):
+    print("Hello"+name)
+greet("Bob")
+#6.Code Snippet 6
+age=int(input("Enter your age:"))
+if age>=18:
+    print("You are eligible to vote.")
+else:
+    print("you are not eligible to vote.")
+#7.Code Snippet 7
+def multiply(a,b):
+    result=a*b
+    return result
+print(multiply(4,5))
+#Code Spinnet 8
+count=10
+while count>0:
+    print(count)
+    count-=1
+print("Countdown Complete!")
+
+
+
+
+
+
